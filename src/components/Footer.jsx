082df922bd055bc2a3ccb4105fcaa62ba37c1c31
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-03 16:46:28 -0500

    Changed link color for readability

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,15 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer mt-auto">
-      Created by
-      <a href="https://ladyaudrey.github.io/portfolio/"> Audrey Evergreene</a>,
-      May, 2023
+    <footer className="footer mt-auto ps-3">
+      Created by{" "}
+      <a
+        className="text-success"
+        href="https://ladyaudrey.github.io/portfolio/"
+      >
+        Audrey Evergreene
+      </a>
+      , May, 2023
     </footer>
   );
 }
