cf0bb2b3d1825c8271be430a2b07fdba9186c43e
LadyAudrey <theaudreygreene@gmail.com>
2023-06-09 19:40:07 -0700

    Deploy debugging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,9 @@ export default function Header(props) {
   const { setButtonSelection, setSelectionMade } = props;
   return (
     <div className="header-container">
-      {/* <img className="info" src="/i.png"></img> */}
       <h1 id="opening-tag" className="fs-1">
         Word game generator
       </h1>
-      {/* I tried adding "className="w-75 h-100 bg-light" and changing it to a button,but tha did not work" */}
       <NavLink to={"/"}>
         <Button
           className="bg-dark p-0 border-0 rounded-circle"
