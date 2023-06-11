5ec2670824bc75c9f3c74be8f8e63463167af6ab
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-11 15:33:09 -0500

    Reduced margin on grid

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -4,7 +4,7 @@ import { Table } from "react-bootstrap";
 export default function Output(props) {
   const { output } = props;
   return (
-    <div className="border border-5 rounded-3 m-5">
+    <div className="border border-5 rounded-3 m-1">
       <div id="print" className="bg-success p-3">
         {output.map((value, index) => {
           return (
